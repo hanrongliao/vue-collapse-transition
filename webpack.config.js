15b1712e00c98b5064e84274597369c914b38cc3
HanrongLiao <hanrongliao@gmail.com>
2018-05-20 15:08:21 +0800

    no sourceMap

--- webpack.config.js
+++ webpack.config.js
@@ -89,7 +89,7 @@ if (process.env.NODE_ENV === 'production') {
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
+      sourceMap: false,
       compress: {
         warnings: false
       }
